b34054f4a3eedf4cc272b5b205a7d9101299d57d
Matvey Firsov <firsov.mat@yandex.ru>
2023-01-05 17:21:02 +0300

    HW

---
+++ Task41/Program.cs
@@ -0,0 +1,32 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int Prompt(string message)
+{
+    System.Console.Write("message");
+    return int.Parse(Console.ReadLine());
+}
+
+
+System.Console.Write("Введите длинну массива > ");
+string inStr = Console.ReadLine();
+int v = Convert.ToInt32(inStr);
+
+int[] numbers = new int[v];
+ int summ = 0;
+for(int i = 0; i < v ; i++)
+{
+    System.Console.WriteLine("Введите число");
+    string point = Console.ReadLine();
+    int a = Convert.ToInt32(point);
+    numbers[i] = a;
+    if(a > 0)
+    {  
+        summ++;
+    }
+    
+}
+System.Console.WriteLine(summ);
+
+
